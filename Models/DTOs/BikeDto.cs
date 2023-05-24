91fb832eb1f58631b8f949164ecfeb7af9993f71
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 00:33:41 +0200

    refactor BikeDto.cs.

--- Models/DTOs/BikeDto.cs
+++ Models/DTOs/BikeDto.cs
@@ -5,30 +5,38 @@ namespace BikeServiceAPI.Models.DTOs;
 public class BikeDto
 {
     public long Id { get; set; }
+    public string VIN { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public string BikeType { get; set; }
+    public int WheelSize { get; set; }
+    public string FrameSize { get; set; }
+    public string State { get; set; }
+    public long UserId { get; set; }
+    public List<string> ServiceHistory { get; set; }
+    public bool Insured { get; set; }
 
-    public string VIN { get; set; } = null!;
-    public string Manufacturer { get; set; } = null!;
-    public string Model { get; set; } = null!;
-    public string Type { get; set; } = null!;
-    public int? WheelSize { get; set; } = null!;
-    public string FrameSize { get; set; } = null!;
-    public string State { get; set; } = null!;
-
-    public string OwnerName { get; set; } = null!;
-    public List<string> ServiceHistory { get; set; } = null!;
-    public bool? Insured { get; set; } = null!;
+    public BikeDto()
+    {
+    }
 
     public BikeDto(Bike bike)
     {
+        Id = bike.Id;
         VIN = bike.VIN;
         Manufacturer = bike.Manufacturer;
         Model = bike.Model;
-        Type = bike.Type.ToString();
+        BikeType = bike.BikeType.ToString();
         WheelSize = bike.WheelSize;
         FrameSize = bike.FrameSize.ToString();
         State = bike.State.ToString();
-        OwnerName = bike.Owner.Name;
-        ServiceHistory = bike.ServiceHistory.Select(ev => $"Start: {ev.Start}, end: {ev.End}, price: {ev.Price}, service event type: {ev.Type.ToString()}, Bike VIN {ev.Bike.VIN}.").ToList();
+        UserId = bike.UserId;
+        ServiceHistory = GetServiceList(bike.ServiceHistory);
         Insured = bike.Insured;
+    }
+
+    private List<string> GetServiceList(List<ServiceEvent> serviceHistory)
+    {
+        return serviceHistory.Select(serviceEvent => serviceEvent.ToString()).ToList();
     }
 }
