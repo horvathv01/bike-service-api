9f09575b5b6bede23032ea4fd23847102f34f203
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 00:42:05 +0200

    refactor Colleague.cs.

--- Models/Entities/Colleague.cs
+++ Models/Entities/Colleague.cs
@@ -4,10 +4,12 @@ namespace BikeServiceAPI.Models.Entities;
 
 public class Colleague : Person
 {
-    public SkillLevel? SkillLevel { get; set; }
+    public SkillLevel SkillLevel { get; set; }
 
     public List<ServiceEvent> ServiceEvents { get; set; } = null!;
-    public Colleague(string name, string email, string password, string phone, SkillLevel? skillLevel = null, string? introduction = null) : base(name, email, password, phone, introduction)
+
+    public Colleague(string name, string email, string password, string phone, SkillLevel skillLevel,
+        string? introduction = null) : base(name, email, password, phone, introduction)
     {
         SkillLevel = skillLevel;
     }
