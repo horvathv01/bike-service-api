92218ed5953a5b6c9856b8a60204c1b9922cf170
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 00:43:27 +0200

    clean User.cs.

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -2,17 +2,13 @@ namespace BikeServiceAPI.Models.Entities;
 
 public class User : Person
 {
-	public bool? Premium { get; set; } = null!;
-	public List<Bike> Bikes { get; set; } = new List<Bike>();
-	public List<Tour>? Tours { get; set; } = null;
-	//public List<ServiceEvent> ServiceEvents { get; set; } = null!;
-	public List<Transaction> TransactionHistory { get; set; } = null!;
-	public List<Bike> InsuredBikes { get; set; } = new List<Bike>();
+	public bool Premium { get; set; }
+	public List<Bike>? Bikes { get; set; } = new List<Bike>();
+	public List<Tour>? Tours { get; set; } = new List<Tour>();
 
-    public User(string name, string email, string password, string phone, string? introduction = null) : base(name, email, password, phone, introduction)
+	public List<Transaction>? TransactionHistory { get; set; } = new List<Transaction>();
+
+	public User(string name, string email, string password, string phone, string? introduction = null) : base(name, email, password, phone, introduction)
     {
-	    Tours = new List<Tour>();
-	    TransactionHistory = new List<Transaction>();
-	    
     }
 }
