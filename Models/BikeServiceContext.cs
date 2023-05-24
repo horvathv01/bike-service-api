c9a2bf77ad4594c309390d89ffaf76fc749a812c
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 00:33:16 +0200

    remove relations, let EntityFramework handle those.

--- Models/BikeServiceContext.cs
+++ Models/BikeServiceContext.cs
@@ -11,42 +11,10 @@ public class BikeServiceContext : DbContext
     public DbSet<BikeNews> BikeNews { get; set; }
     public DbSet<ServiceEvent> ServiceEvents { get; set; }
     public DbSet<Tour> Tours { get; set; }
-
     public DbSet<Transaction> Transactions { get; set; }
-    
     public DbSet<Part> Parts { get; set; }
 
     public BikeServiceContext(DbContextOptions<BikeServiceContext> options) : base(options)
     {
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<ServiceEvent>()
-            .HasOne(ev => ev.Bike)
-            .WithMany(bike => bike.ServiceHistory);
-            //.HasForeignKey(ev => ev.Bike.Id);
-
-            modelBuilder.Entity<ServiceEvent>()
-                .HasOne(ev => ev.Colleague)
-                .WithMany(coll => coll.ServiceEvents);
-            //.HasForeignKey(ev => ev.Colleague.Id);
-
-            modelBuilder.Entity<Bike>()
-                .HasOne(bike => bike.Owner)
-                .WithMany(owner => owner.Bikes);
-            //.HasForeignKey(bike => bike.Owner.Id);
-            
-            modelBuilder.Entity<Tour>()
-                .HasMany(tour => tour.Participants)
-                .WithMany(participant => participant.Tours);
-
-            modelBuilder.Entity<Transaction>()
-                .HasOne(trans => trans.User)
-                .WithMany(user => user.TransactionHistory);
-
-            modelBuilder.Entity<Part>()
-                .HasOne(part => part.Transaction)
-                .WithMany(trans => trans.PurchasedItems);
     }
 }
