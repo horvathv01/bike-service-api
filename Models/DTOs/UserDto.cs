202ce96b7c4695fc4a6fefa0bfd4f789bc8d0de9
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 00:43:44 +0200

    refactor UserDto.cs.

--- Models/DTOs/UserDto.cs
+++ Models/DTOs/UserDto.cs
@@ -5,32 +5,34 @@ namespace BikeServiceAPI.Models.DTOs;
 public class UserDto
 {
     public long Id { get; set; }
-
-    public string Name { get; private set; } = null!;
-    public string Email { get; private set; } = null!;
-    public string Password { get; private set; } = null!;
-    public string Phone { get; private set; } = null!;
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
     public string? Introduction { get; set; }
-    
-    public bool? Premium { get; set; } = null!;
-    public List<BikeDto> Bikes { get; set; } = new List<BikeDto>();
-    public List<string>? Tours { get; set; } = null;
-    //public List<ServiceEvent> ServiceEvents { get; set; } = null!;
-    public List<string> TransactionHistory { get; set; } = null!;
-    public List<string> InsuredBikes { get; set; } = new List<string>();
+    public bool Premium { get; set; }
+    public List<BikeDto>? Bikes { get; set; } = new List<BikeDto>();
+    public List<string>? Tours { get; set; } = new List<string>();
+    public List<string>? TransactionHistory { get; set; } = new List<string>();
+
+    public UserDto()
+    {
+    }
 
     public UserDto(User user)
     {
         Name = user.Name;
         Email = user.Email;
         Password = user.Password;
-        Phone = user.Password;
+        Phone = user.Phone;
         Introduction = user.Introduction;
-        user.Premium = Premium;
+
+        Premium = user.Premium;
         Bikes = user.Bikes.Select(bike => new BikeDto(bike)).ToList();
-        Tours = user.Tours.Select(tour => $"{tour.Name}, {tour.Type}, {tour.Difficulty}, {tour.Start} - {tour.End}").ToList();
-        TransactionHistory = user.TransactionHistory.SelectMany(ev => ev.PurchasedItems.Select(item => $"Model: {item.Model}, Price: {item.Price}.")).ToList();
-        InsuredBikes = user.InsuredBikes.Select(bike => $"VIN: {bike.VIN}, Insured: {bike.Insured}, Manufacturer: {bike.Manufacturer}," +
-                                                        $" Model: {bike.Model}, Type: {bike.Type.ToString()}, Wheel size: {bike.WheelSize}.").ToList();
+        Tours = ParseTours(user.Tours);
+        TransactionHistory = ParseTransactionHistory(user.TransactionHistory);
     }
+
+    public List<string> ParseTours(List<Tour> tours) => tours.Select(tour => tour.ToString()).ToList();
+    public List<string> ParseTransactionHistory(List<Transaction> transactions) => transactions.Select(transaction => transaction.ToString()).ToList();
 }
