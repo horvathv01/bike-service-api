5b15789d5da16b7bbca1e9580f8f8a6f969363b5
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 00:31:45 +0200

    refactor BikeService.cs using BikeDto.

--- Services/BikeService.cs
+++ Services/BikeService.cs
@@ -1,5 +1,7 @@
 ﻿using BikeServiceAPI.Models;
+using BikeServiceAPI.Models.DTOs;
 using BikeServiceAPI.Models.Entities;
+using BikeServiceAPI.Models.Mappers;
 using Microsoft.EntityFrameworkCore;
 
 namespace BikeServiceAPI.Services;
@@ -7,60 +9,59 @@ namespace BikeServiceAPI.Services;
 public class BikeService : IBikeService
 {
     private readonly BikeServiceContext _context;
+    private readonly IMapper<Bike, BikeDto> _bikeDtoMapper;
 
-    public BikeService(BikeServiceContext context)
+    public BikeService(BikeServiceContext context, IMapper<Bike, BikeDto> bikeDtoMapper)
     {
         _context = context;
+        _bikeDtoMapper = bikeDtoMapper;
     }
 
-    public async Task AddBike(Bike bike)
+    public async Task<int> AddBike(BikeDto bikeDto)
     {
-        long id = await _context.Bikes.CountAsync() + 1;
-        bike.Id = id;
+        var bike = _bikeDtoMapper.ToEntity(bikeDto);
         _context.Bikes.Add(bike);
-        await _context.SaveChangesAsync();
+        return await _context.SaveChangesAsync();
     }
 
-    public async Task<Bike> GetBikeById(long id)
+    public async Task<BikeDto> GetBikeById(long id)
+    {
+        var bike = await GetBikeEntityById(id) ?? throw new InvalidOperationException("bike with id not exist");
+
+        return _bikeDtoMapper.ToDto(bike);
+    }
+
+    public async Task<int> UpdateBike(BikeDto bikeDto)
+    {
+        Bike bike = await GetBikeEntityById(bikeDto.Id) ??
+                    throw new InvalidOperationException("bike with id not exist");
+        var updateBike = _bikeDtoMapper.ToEntity(bikeDto);
+
+        _context.Entry(bike).CurrentValues.SetValues(updateBike);
+        return await _context.SaveChangesAsync();
+    }
+
+    public async Task<int> DeleteBike(long id)
+    {
+        Bike bike = await GetBikeEntityById(id) ?? throw new InvalidOperationException("bike with id not exist");
+        _context.Bikes.Remove(bike);
+        return await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<BikeDto>> GetAllBikes()
+    {
+        var bikeList = await _context.Bikes
+            .Include(bike => bike.ServiceHistory)
+            .ToListAsync();
+        return bikeList.Select(bike => _bikeDtoMapper.ToDto(bike)).ToList();
+    }
+
+    private async Task<Bike?> GetBikeEntityById(long id)
     {
         var bike = await _context.Bikes
-            .Include(bike => bike.Owner)
             .Include(bike => bike.ServiceHistory)
             .FirstOrDefaultAsync(bike => bike.Id == id);
 
-        if (bike == null)
-        {
-            Console.WriteLine("Bike not found!");
-            return null;
-        }
-
         return bike;
-    }
-
-    public async Task UpdateBike(Bike newBike, long id)
-    {
-        Bike bike = await GetBikeById(id);
-        if (bike == null)
-        {
-            return;
-        }
-        
-        _context.Entry(bike).CurrentValues.SetValues(newBike);
-        await _context.SaveChangesAsync();
-    }
-
-    public async Task DeleteBike(long id)
-    {
-        Bike bike = await GetBikeById(id);
-        _context.Bikes.Remove(bike);
-        await _context.SaveChangesAsync();
-    }
-
-    public async Task<List<Bike>> GetAllBikes()
-    {
-        return await _context.Bikes
-            .Include(bike => bike.Owner)
-            .Include(bike => bike.ServiceHistory)
-            .ToListAsync();
     }
 }
