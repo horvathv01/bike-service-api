e2fb8709cb2479fe3c7000acfee54a1d1db37a6d
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 00:37:05 +0200

    refactor Transaction.cs.

--- Models/Entities/Transaction.cs
+++ Models/Entities/Transaction.cs
@@ -6,7 +6,12 @@ public class Transaction
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
-    public double? TotalPrice { get; set; } = null!;
-    public User? User { get; set; } = null!;
-    public List<Part> PurchasedItems { get; set; }
+    public double? TotalPrice { get; set; }
+    public User? User { get; set; }
+    public List<Part> PurchasedItems { get; set; } = new List<Part>();
+    
+    public override string ToString()
+    {
+        return string.Concat(PurchasedItems.Select(part => part.ToString()));
+    }
 }
