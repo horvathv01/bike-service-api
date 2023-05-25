ccdbe7022374f62eeca0d404eea19eabcf28add5
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 14:38:06 +0200

    create part dto, and add mapper function into part entity.

--- Models/Entities/Part.cs
+++ Models/Entities/Part.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using BikeServiceAPI.Models.DTOs;
 
 namespace BikeServiceAPI.Models.Entities;
 
@@ -6,15 +7,24 @@ public class Part
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
-
     public string Make { get; set; } = null!;
     public string Model { get; set; } = null!;
-
     public double Price { get; set; }
+    public string Description { get; set; }
 
-    public string Description { get; set; } = null!;
+    public Part()
+    {
+        
+    }
 
-    public Transaction Transaction { get; set; } = null!;
+    public Part(PartDto dto)
+    {
+        Id = dto.Id;
+        Make = dto.Make;
+        Model = dto.Model;
+        Price = dto.Price;
+        Description = dto.Description;
+    }
 
     public override string ToString()
     {
