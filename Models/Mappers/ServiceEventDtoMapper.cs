776cdda86192ba9560751551ff894aa85ef11c38
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 11:42:36 +0200

    create ServiceEventDtoMapper.cs.

---
+++ Models/Mappers/ServiceEventDtoMapper.cs
@@ -0,0 +1,28 @@
+﻿using BikeServiceAPI.Enums;
+using BikeServiceAPI.Models.DTOs;
+using BikeServiceAPI.Models.Entities;
+
+namespace BikeServiceAPI.Models.Mappers;
+
+public class ServiceEventDtoMapper : IMapper<ServiceEvent,ServiceEventDto>
+{
+    public ServiceEvent ToEntity(ServiceEventDto dto)
+    {
+        var serviceEvent = new ServiceEvent
+        {
+            Id = dto.Id,
+            Type = Enum.Parse<ServiceEventType>(dto.Type),
+            Start = dto.Start,
+            End = dto.End,
+            Price = dto.Price,
+            BikeId = dto.BikeId,
+            ColleagueId = dto.ColleagueId
+        };
+        return serviceEvent;
+    }
+
+    public ServiceEventDto ToDto(ServiceEvent entity)
+    {
+        return new ServiceEventDto(entity);
+    }
+}
