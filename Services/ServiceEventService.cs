6d857291067127a68c4d69cde2f926789e3e08de
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-05-25 11:43:12 +0200

    implement IServiceEventService interface into ServiceEventService.cs.

---
+++ Services/ServiceEventService.cs
@@ -0,0 +1,31 @@
+﻿using BikeServiceAPI.Models.DTOs;
+
+namespace BikeServiceAPI.Services;
+
+public class ServiceEventService : IServiceEventService
+{
+    public Task<int> AddServiceEvent(ServiceEventDto serviceEventDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceEventDto> GetServiceEventById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> UpdateServiceEvent(ServiceEventDto serviceEventDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> DeleteServiceEvent(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ServiceEventDto>> GetAllServiceEvent()
+    {
+        throw new NotImplementedException();
+    }
+}
